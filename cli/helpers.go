242292bcddab216c9b5a28285c459c305cdc12db
Aníbal Deboni Neto <anibal.deboni@creditas.com>
2024-01-26 11:00:03 -0300

    refactor cli exit function

--- cli/helpers.go
+++ cli/helpers.go
@@ -6,7 +6,16 @@ import (
 	"rapper/ui"
 )
 
-func ExitOnError(message string, arg ...any) {
-	fmt.Println(ui.QuitTextStyle.Render(fmt.Sprintf(message+"\n", arg...)))
-	os.Exit(1)
+func Exit(message any, arg ...any) {
+	switch message.(type) {
+	case string:
+		fmt.Println(ui.QuitTextStyle.Render(fmt.Sprintf(message.(string), arg...)))
+		os.Exit(0)
+	case error:
+		fmt.Println(ui.QuitTextStyle.Render(fmt.Sprintf(message.(error).Error()+"\n", arg...)))
+		os.Exit(1)
+	default:
+		fmt.Println(ui.QuitTextStyle.Render(fmt.Sprintf("%v\n", message)))
+		os.Exit(1)
+	}
 }
--- main.go
+++ main.go
@@ -17,54 +17,52 @@ func main() {
 
 	config, err := files.Config(path)
 	if err != nil {
-		cli.ExitOnError(err.Error())
+		cli.Exit(err)
 	}
 
 	csvPath, err := files.ChooseFile(path)
 	if err != nil {
-		cli.ExitOnError(err.Error())
+		cli.Exit(err)
 	}
 
 	csv, err := files.MapCSV(csvPath)
 	if err != nil {
-		cli.ExitOnError(err.Error())
+		cli.Exit(err)
 	}
 
 	filteredCSV := files.FilterCSV(csv, config.CSV)
 	hg := web.NewHttpGateway(config.Token, config.Path.Method, config.Path.Template, config.Payload.Template)
 
 	if err := cli.Run(filteredCSV, hg); err != nil {
-		cli.ExitOnError(err.Error())
+		cli.Exit(err)
 	}
 }
 
 func handleArgs() string {
 	cwd, err := os.Getwd()
 	if err != nil {
-		cli.ExitOnError(err.Error())
+		cli.Exit(err)
 	}
 	if len(os.Args) > 1 {
 		arg := os.Args[1]
 		switch arg {
 		case "help":
-			usage()
-			os.Exit(0)
+			cli.Exit(usage())
 		case "version":
-			fmt.Println(AppVersion)
-			os.Exit(0)
+			cli.Exit(AppVersion)
 		}
 		if files.IsDir(arg) {
 			return arg
 		} else {
-			cli.ExitOnError("%s is not a directory", ui.Bold(arg))
+			cli.Exit(fmt.Errorf("%s is not a directory", ui.Bold(arg)))
 		}
 	}
 
 	return cwd
 }
 
-func usage() {
-	fmt.Printf(
+func usage() string {
+	return fmt.Sprintf(
 		"%s (%s)\n\n"+
 			"You must always have a %s file in the folder you will run the app.\n"+
 			"If you don't point to a specific directory the current one will be used.\n\n"+
@@ -72,6 +70,6 @@ func usage() {
 		ui.Bold(AppName),
 		AppVersion,
 		ui.Italic("config.yml"),
-		ui.Bold(AppName+" [<folder-with-csv>]\n"),
+		ui.Bold(AppName+" [<folder-with-csv>]"),
 	)
 }
