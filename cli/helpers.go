0e009289a39ab62fb258bc665f7904cf81c57ab3
Aníbal Deboni Neto <anibal.deboni@creditas.com>
2024-01-26 17:48:13 -0300

    fix exit message

--- cli/helpers.go
+++ cli/helpers.go
@@ -9,11 +9,16 @@ import (
 func Exit(message any, arg ...any) {
 	switch message.(type) {
 	case string:
+		if len(message.(string)) == 0 {
+			os.Exit(0)
+		}
 		fmt.Println(ui.QuitTextStyle.Render(fmt.Sprintf(message.(string), arg...)))
 		os.Exit(0)
 	case error:
 		fmt.Println(ui.QuitTextStyle.Render(fmt.Sprintf(message.(error).Error()+"\n", arg...)))
 		os.Exit(1)
+	case nil:
+		os.Exit(0)
 	default:
 		fmt.Println(ui.QuitTextStyle.Render(fmt.Sprintf("%v\n", message)))
 		os.Exit(1)
