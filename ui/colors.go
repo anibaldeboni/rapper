b1e4a714a7bb763be0c2e73d9199a30f9a709f34
Aníbal Deboni Neto <anibal.deboni@creditas.com>
2024-01-31 19:08:10 -0300

    add green color

--- ui/colors.go
+++ ui/colors.go
@@ -8,7 +8,7 @@ import (
 
 func toString(s any) string {
 	var str string
-	switch s.(type) {
+	switch s := s.(type) {
 	case int:
 		str = fmt.Sprintf("%d", s)
 	default:
@@ -32,4 +32,8 @@ func Italic(s any) string {
 func Red(s any) string {
 	r := color.New(color.FgRed).SprintFunc()
 	return r(toString(s))
+}
+func Green(s any) string {
+	g := color.New(color.FgHiGreen).SprintFunc()
+	return g(toString(s))
 }
